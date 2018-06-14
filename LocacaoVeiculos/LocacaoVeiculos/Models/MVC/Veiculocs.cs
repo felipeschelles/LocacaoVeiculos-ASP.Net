5691a53effdad14c9c662d14fc525203d510a63d
Guilherme Lindbeck Fernandes <guilinfer@gmail.com>
2018-06-14 16:29:43 -0300

    criando modelo veiculo

--- LocacaoVeiculos/LocacaoVeiculos/Models/MVC/Veiculocs.cs
+++ LocacaoVeiculos/LocacaoVeiculos/Models/MVC/Veiculocs.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace LocacaoVeiculos.Models
+namespace LocacaoVeiculos.Models.MVC
 {
-    public class Veiculo
+    public class Veiculocs
     {
+
         public int VeiculoID { get; set; }
 
         public string Nome { get; set; }
 
         public string Fabricante { get; set; }
-        
+
         public string Cor { get; set; }
 
         public int Ano { get; set; }
-
-
-
     }
 }
